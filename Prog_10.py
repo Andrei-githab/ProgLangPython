6aea026a88605b3bf5dfce7cf14d6ec5b7851de5
Andru-hub <67545441+Andru-hub@users.noreply.github.com>
2022-05-22 00:20:07 +0300

    Update Prog_10.py

--- Prog_10.py
+++ Prog_10.py
@@ -1,14 +1,20 @@
 import datetime
 
+
 class Loger:
-    global now
-    naw = datetime.datetime.now()
+    def __init__(self, x):
+        self.x = x
+
     def startl(self):
+        now = datetime.datetime.now()
         print("<Запуск программы>[{}]/[{}]/[{}]/[{}:{}]".format(now.day, now.month, now.year, now.hour, now.minute))
-        
+
     def X_peremeshchenie(self):
+        now = datetime.datetime.now()
         print("<Запуск программы>[{}]/[{}]/[{}]/[{}:{}]".format(now.day, now.month, now.year, now.hour, now.minute))
+        return
 
 
 startloger = Loger()
 startloger.startl()
+startloger.X_peremeshchenie()
