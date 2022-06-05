b0743fbe879d0fce6d557b5638f8b3d4d593f3cd
Andru-hub <67545441+Andru-hub@users.noreply.github.com>
2022-06-05 14:46:41 +0300

    Update Prog_08.py

--- Prog_08.py
+++ Prog_08.py
@@ -35,20 +35,21 @@ plt.ylim(min(x1,x2,x3,x4,x5-r,x6,y1,y2,y3,y4,y5-r,y6,-x1,-x2,-x3,-x4,-(x5-r),-x6
 plt.gca().add_patch(Rectangle((-10000,-10000), 20000, 20000, facecolor="cyan"))
 l = max(x3,x4)-min(x3,x4)
 d = max(y3,y4)-min(y3,y4)
-#Р·Р°Р»С‚РІРєР° РЅСѓР¶РЅРѕР№ РѕР±Р»Р°СЃС‚Рё
-def cerc():
-    #circle
+
+
+def print_circle():
+
     ra = (x6-x5)*(x6-x5)+(y6-y5)*(y6-y5)
 
-    if r*r<ra:
+    if r*r < ra:
         plt.gca().add_patch(plt.Circle([x5,y5], r, color='w',))
     else:
         plt.gca().add_patch(Rectangle((-10000,y5+r), 20000, 20000, facecolor="w"))
         plt.gca().add_patch(Rectangle((-10000,y5-r), 20000, -20000, facecolor="w"))
         plt.gca().add_patch(Rectangle((x5+r,y5+r), 20000, -20000, facecolor="w"))
         plt.gca().add_patch(Rectangle((x5-r,y5-r), -20000, 20000, facecolor="w"))
-        xp=np.array([x5-r,x5-r*0.98,x5-r*0.96,x5-r*0.94,x5-r*0.92,x5-r*0.9,x5-r*0.8,x5-r*0.7,x5-r*0.6,x5-r*0.5,x5-r*0.4,x5-r*0.3,x5-r*0.2,x5-r*0.1,x5,x5+r*0.1,x5+r*0.2,x5+r*0.3,x5+r*0.4,x5+r*0.5,x5+r*0.6,x5+r*0.7,x5+r*0.8,x5+r*0.9,x5+r*0.92,x5+r*0.94,x5+r*0.96,x5+r*0.98,x5+r])
-        yp=np.array([y5,y5+r*0.2,y5+r*0.28,y5+r*0.34,y5+r*0.4,y5+r*0.43,y5+r*0.6,y5+r*0.71,y5+r*0.8,y5+r*0.86,y5+r*0.91,y5+r*0.95,y5+r*0.98,y5+r*0.99,y5+r,y5+r*0.99,y5+r*0.98,y5+r*0.95,y5+r*0.91,y5+r*0.86,y5+r*0.8,y5+r*0.71,y5+r*0.6,y5+r*0.43,y5+r*0.4,y5+r*0.34,y5+r*0.28,y5+r*0.2,y5])
+        xp = np.array([x5-r,x5-r*0.98,x5-r*0.96,x5-r*0.94,x5-r*0.92,x5-r*0.9,x5-r*0.8,x5-r*0.7,x5-r*0.6,x5-r*0.5,x5-r*0.4,x5-r*0.3,x5-r*0.2,x5-r*0.1,x5,x5+r*0.1,x5+r*0.2,x5+r*0.3,x5+r*0.4,x5+r*0.5,x5+r*0.6,x5+r*0.7,x5+r*0.8,x5+r*0.9,x5+r*0.92,x5+r*0.94,x5+r*0.96,x5+r*0.98,x5+r])
+        yp = np.array([y5,y5+r*0.2,y5+r*0.28,y5+r*0.34,y5+r*0.4,y5+r*0.43,y5+r*0.6,y5+r*0.71,y5+r*0.8,y5+r*0.86,y5+r*0.91,y5+r*0.95,y5+r*0.98,y5+r*0.99,y5+r,y5+r*0.99,y5+r*0.98,y5+r*0.95,y5+r*0.91,y5+r*0.86,y5+r*0.8,y5+r*0.71,y5+r*0.6,y5+r*0.43,y5+r*0.4,y5+r*0.34,y5+r*0.28,y5+r*0.2,y5])
         plt.fill_between(xp,yp,y2=y5+r,color = "w")
         yp = np.array(
             [y5, y5 - r * 0.2, y5 - r * 0.28, y5 - r * 0.34, y5 - r * 0.4, y5 - r * 0.43, y5 - r * 0.6, y5 - r * 0.71,
@@ -59,9 +60,8 @@ def cerc():
         plt.fill_between(xp, yp, y2=y5 - r, color="w")
 
 
-
-def linec():
-    #line ??? hard
+def print_line():
+    
     if(x1!=x2):
         kx = (x1 - x2) / (y1 - y2)
         bx = -y2 * (x1 - x2) / (y1 - y2) + x2
@@ -118,16 +118,16 @@ def linec():
                         plt.fill_between([bx, 0], [0, by], color="cyan")
                         plt.fill_betweenx([by, 10000], [0, 10000 * 1 / ky + by], color="w")
     else:
-        if(x6>x1):
+        if(x6 > x1):
             plt.gca().add_patch(Rectangle((x6, 10000), 20000, -20000, facecolor="w"))
         else:
             plt.gca().add_patch(Rectangle((x6, 10000), -20000, -20000, facecolor="w"))
 
 
+def print_recctangle():
 
-def recc():
-    #rec yes
-    if((min(x3,x4)<=x6<=max(x3,x4)) and((min(y3,y4)<=y6<=max(y3,y4)))):
+    if (min(x3, x4) <= x6 <= max(x3, x4)) and (min(y3, y4) <= y6 <= max(y3, y4)):
+
         plt.gca().add_patch(Rectangle((-10000, max(y3,y4)), 20000, 20000, facecolor="w"))
         plt.gca().add_patch(Rectangle((-10000, max(y3,y4)), min(x3,x4)+10000, -20000, facecolor="w"))
         plt.gca().add_patch(Rectangle((max(x3,x4), max(y3, y4)), 20000, -20000, facecolor="w"))
@@ -138,21 +138,21 @@ def recc():
         print(2)
 
 
-if x1==x2:
-    line([x1, x1],[-10000, 10000])
+if x1 == x2:
+    line([x1, x1], [-10000, 10000])
 else:
-    line([-10000,10000],[(-10000-x2)/(x1-x2)*(y1-y2)+y2,(10000-x2)/(x1-x2)*(y1-y2)+y2])
+    line([-10000, 10000], [(-10000-x2)/(x1-x2)*(y1-y2)+y2, (10000-x2)/(x1-x2)*(y1-y2)+y2])
 
-linec()
-cerc()
-recc()
+print_line()
+print_circle()
+print_recctangle()
 
 x, y = [x1, x2], [y1, y2]
-line(x,y)
+line(x, y)
 
-x = [min(x3, x4), min(y3,y4)]
-rectangle(x,l,d)
-x =[x5,y5]
-circle(x,r)
-plt.gca().scatter(x6,y6)
+x = [min(x3, x4), min(y3, y4)]
+rectangle(x, l, d)
+x = [x5, y5]
+circle(x, r)
+plt.gca().scatter(x6, y6)
 plt.show()
