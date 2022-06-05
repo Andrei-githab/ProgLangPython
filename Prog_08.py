f3999957e99323a9b6317c6585a7fb15b24c2113
Andru-hub <67545441+Andru-hub@users.noreply.github.com>
2022-06-05 15:11:35 +0300

    Лабораторная работа 8

--- Prog_08.py
+++ Prog_08.py
@@ -1,7 +1,12 @@
+"""
+Лабораторная работа 8 
+Написать программу попадания точки в области 
+(визуализированный вариант)
+"""
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
-import keyboard
+
 plt.figure(figsize=(8, 8))
 plt.gca().spines['left'].set_position('center')
 plt.gca().spines['bottom'].set_position('center')
@@ -13,64 +18,89 @@ def line(x1_line, x2_line):
     plt.plot(x1_line, x2_line, color="k")
 
 
-def rectangle(x_rectangle, l, d):
+def rectangle(x_rectangle, l_rectangle, d_rectangle):
     currentAxis = plt.gca()
-    currentAxis.add_patch(Rectangle((x_rectangle), l, d, facecolor="None", ec='k'))
+    currentAxis.add_patch(Rectangle(x_rectangle, l_rectangle, d_rectangle, facecolor="None", ec='k'))
 
 
-def circle(x,r):
-    circle1 = plt.Circle((x), r, color='None', ec='k')
+def circle(x_circle, r_circle):
+    circle1 = plt.Circle(x_circle, r_circle, color='None', ec='k')
     plt.gca().add_patch(circle1)
 
 
+x_l, y_l, x_ll, y_ll = map(int, input().split())
+x_rec, y_rec, x_recc, y_recc = map(int, input().split())
+x_cer, y_cer, r_cer = map(int, input().split())
+x_toh, y_toh = map(int, input().split())
+plt.xlim(
+    min(x_l, x_ll, x_rec, x_recc, x_cer - r_cer, x_toh, y_l, y_ll, y_rec, y_recc, y_cer - r_cer, y_toh, -x_l, -x_ll,
+        -x_rec, -x_recc, -(x_cer - r_cer), -x_toh, -y_l, -y_ll, -y_rec, -y_recc, -(y_cer - r_cer), -y_toh,
+        y_cer + r_cer, y_cer - r_cer, -y_cer - r_cer, -y_cer + r_cer) - 3,
+    max(x_l, x_ll, x_rec, x_recc, x_cer - r_cer, x_toh, y_l, y_ll, y_rec, y_recc, y_cer - r_cer, y_toh, -x_l, -x_ll,
+        -x_rec, -x_recc, -(x_cer - r_cer), -x_toh, -y_l, -y_ll, -y_rec, -y_recc, -(y_cer - r_cer), -y_toh,
+        y_cer + r_cer, y_cer - r_cer, -y_cer - r_cer, -y_cer + r_cer) + 3)
+plt.ylim(
+    min(x_l, x_ll, x_rec, x_recc, x_cer - r_cer, x_toh, y_l, y_ll, y_rec, y_recc, y_cer - r_cer, y_toh, -x_l, -x_ll,
+        -x_rec, -x_recc, -(x_cer - r_cer), -x_toh, -y_l, -y_ll, -y_rec, -y_recc, -(y_cer - r_cer), -y_toh,
+        y_cer + r_cer, y_cer - r_cer, -y_cer - r_cer, -y_cer + r_cer) - 3,
+    max(x_l, x_ll, x_rec, x_recc, x_cer - r_cer, x_toh, y_l, y_ll, y_rec, y_recc, y_cer - r_cer, y_toh, -x_l, -x_ll,
+        -x_rec, -x_recc, -(x_cer - r_cer), -x_toh, -y_l, -y_ll, -y_rec, -y_recc, -(y_cer - r_cer), -y_toh,
+        y_cer + r_cer, y_cer - r_cer, -y_cer - r_cer, -y_cer + r_cer) + 3)
 
-
-x1,y1,x2,y2 = map(int,input().split())
-x3, y3, x4, y4 = map(int,input().split())
-x5, y5, r = map(int,input().split())
-x6,y6 = map(int,input().split())
-plt.xlim(min(x1,x2,x3,x4,x5-r,x6,y1,y2,y3,y4,y5-r,y6,-x1,-x2,-x3,-x4,-(x5-r),-x6,-y1,-y2,-y3,-y4,-(y5-r),-y6,y5+r,y5-r,-y5-r,-y5+r)-3, max(x1,x2,x3,x4,x5-r,x6,y1,y2,y3,y4,y5-r,y6,-x1,-x2,-x3,-x4,-(x5-r),-x6,-y1,-y2,-y3,-y4,-(y5-r),-y6,y5+r,y5-r,-y5-r,-y5+r)+3)
-plt.ylim(min(x1,x2,x3,x4,x5-r,x6,y1,y2,y3,y4,y5-r,y6,-x1,-x2,-x3,-x4,-(x5-r),-x6,-y1,-y2,-y3,-y4,-(y5-r),-y6,y5+r,y5-r,-y5-r,-y5+r)-3, max(x1,x2,x3,x4,x5-r,x6,y1,y2,y3,y4,y5-r,y6,-x1,-x2,-x3,-x4,-(x5-r),-x6,-y1,-y2,-y3,-y4,-(y5-r),-y6,y5+r,y5-r,-y5-r,-y5+r)+3)
-
-plt.gca().add_patch(Rectangle((-10000,-10000), 20000, 20000, facecolor="cyan"))
-l = max(x3,x4)-min(x3,x4)
-d = max(y3,y4)-min(y3,y4)
+plt.gca().add_patch(Rectangle((-10000, -10000), 20000, 20000, facecolor="cyan"))
+l_tangle = max(x_rec, x_recc) - min(x_rec, x_recc)
+d_tangle = max(y_rec, y_recc) - min(y_rec, y_recc)
 
 
 def print_circle():
+    ra = (x_toh - x_cer) * (x_toh - x_cer) + (y_toh - y_cer) * (y_toh - y_cer)
 
-    ra = (x6-x5)*(x6-x5)+(y6-y5)*(y6-y5)
-
-    if r*r < ra:
-        plt.gca().add_patch(plt.Circle([x5,y5], r, color='w',))
+    if r_cer * r_cer < ra:
+        plt.gca().add_patch(plt.Circle([x_cer, y_cer], r_cer, color='w', ))
     else:
-        plt.gca().add_patch(Rectangle((-10000,y5+r), 20000, 20000, facecolor="w"))
-        plt.gca().add_patch(Rectangle((-10000,y5-r), 20000, -20000, facecolor="w"))
-        plt.gca().add_patch(Rectangle((x5+r,y5+r), 20000, -20000, facecolor="w"))
-        plt.gca().add_patch(Rectangle((x5-r,y5-r), -20000, 20000, facecolor="w"))
-        xp = np.array([x5-r,x5-r*0.98,x5-r*0.96,x5-r*0.94,x5-r*0.92,x5-r*0.9,x5-r*0.8,x5-r*0.7,x5-r*0.6,x5-r*0.5,x5-r*0.4,x5-r*0.3,x5-r*0.2,x5-r*0.1,x5,x5+r*0.1,x5+r*0.2,x5+r*0.3,x5+r*0.4,x5+r*0.5,x5+r*0.6,x5+r*0.7,x5+r*0.8,x5+r*0.9,x5+r*0.92,x5+r*0.94,x5+r*0.96,x5+r*0.98,x5+r])
-        yp = np.array([y5,y5+r*0.2,y5+r*0.28,y5+r*0.34,y5+r*0.4,y5+r*0.43,y5+r*0.6,y5+r*0.71,y5+r*0.8,y5+r*0.86,y5+r*0.91,y5+r*0.95,y5+r*0.98,y5+r*0.99,y5+r,y5+r*0.99,y5+r*0.98,y5+r*0.95,y5+r*0.91,y5+r*0.86,y5+r*0.8,y5+r*0.71,y5+r*0.6,y5+r*0.43,y5+r*0.4,y5+r*0.34,y5+r*0.28,y5+r*0.2,y5])
-        plt.fill_between(xp,yp,y2=y5+r,color = "w")
+        plt.gca().add_patch(Rectangle((-10000, y_cer + r_cer), 20000, 20000, facecolor="w"))
+        plt.gca().add_patch(Rectangle((-10000, y_cer - r_cer), 20000, -20000, facecolor="w"))
+        plt.gca().add_patch(Rectangle((x_cer + r_cer, y_cer + r_cer), 20000, -20000, facecolor="w"))
+        plt.gca().add_patch(Rectangle((x_cer - r_cer, y_cer - r_cer), -20000, 20000, facecolor="w"))
+        xp = np.array(
+            [x_cer - r_cer, x_cer - r_cer * 0.98, x_cer - r_cer * 0.96, x_cer - r_cer * 0.94, x_cer - r_cer * 0.92,
+             x_cer - r_cer * 0.9, x_cer - r_cer * 0.8, x_cer - r_cer * 0.7, x_cer - r_cer * 0.6, x_cer - r_cer * 0.5,
+             x_cer - r_cer * 0.4, x_cer - r_cer * 0.3, x_cer - r_cer * 0.2, x_cer - r_cer * 0.1, x_cer,
+             x_cer + r_cer * 0.1, x_cer + r_cer * 0.2, x_cer + r_cer * 0.3, x_cer + r_cer * 0.4, x_cer + r_cer * 0.5,
+             x_cer + r_cer * 0.6, x_cer + r_cer * 0.7, x_cer + r_cer * 0.8, x_cer + r_cer * 0.9, x_cer + r_cer * 0.92,
+             x_cer + r_cer * 0.94, x_cer + r_cer * 0.96, x_cer + r_cer * 0.98, x_cer + r_cer])
+        yp = np.array([y_cer, y_cer + r_cer * 0.2, y_cer + r_cer * 0.28, y_cer + r_cer * 0.34, y_cer + r_cer * 0.4,
+                       y_cer + r_cer * 0.43, y_cer + r_cer * 0.6, y_cer + r_cer * 0.71, y_cer + r_cer * 0.8,
+                       y_cer + r_cer * 0.86, y_cer + r_cer * 0.91, y_cer + r_cer * 0.95, y_cer + r_cer * 0.98,
+                       y_cer + r_cer * 0.99, y_cer + r_cer, y_cer + r_cer * 0.99, y_cer + r_cer * 0.98,
+                       y_cer + r_cer * 0.95, y_cer + r_cer * 0.91, y_cer + r_cer * 0.86, y_cer + r_cer * 0.8,
+                       y_cer + r_cer * 0.71, y_cer + r_cer * 0.6, y_cer + r_cer * 0.43, y_cer + r_cer * 0.4,
+                       y_cer + r_cer * 0.34, y_cer + r_cer * 0.28, y_cer + r_cer * 0.2, y_cer])
+        plt.fill_between(xp, yp, y2=y_cer + r_cer, color="w")
         yp = np.array(
-            [y5, y5 - r * 0.2, y5 - r * 0.28, y5 - r * 0.34, y5 - r * 0.4, y5 - r * 0.43, y5 - r * 0.6, y5 - r * 0.71,
-             y5 - r * 0.8, y5 - r * 0.86, y5 - r * 0.91, y5 - r * 0.95, y5 - r * 0.98, y5 - r * 0.99, y5 - r,
-             y5 - r * 0.99, y5 - r * 0.98, y5 - r * 0.95, y5 - r * 0.91, y5 - r * 0.86, y5 - r * 0.8, y5 - r * 0.71,
-             y5 - r * 0.6, y5 - r * 0.43, y5 - r * 0.4, y5 - r * 0.34, y5 - r * 0.28, y5 - r * 0.2, y5])
+            [y_cer, y_cer - r_cer * 0.2, y_cer - r_cer * 0.28, y_cer - r_cer * 0.34, y_cer - r_cer * 0.4,
+             y_cer - r_cer * 0.43, y_cer - r_cer * 0.6, y_cer - r_cer * 0.71,
+             y_cer - r_cer * 0.8, y_cer - r_cer * 0.86, y_cer - r_cer * 0.91, y_cer - r_cer * 0.95,
+             y_cer - r_cer * 0.98, y_cer - r_cer * 0.99, y_cer - r_cer,
+             y_cer - r_cer * 0.99, y_cer - r_cer * 0.98, y_cer - r_cer * 0.95, y_cer - r_cer * 0.91,
+             y_cer - r_cer * 0.86, y_cer - r_cer * 0.8, y_cer - r_cer * 0.71,
+             y_cer - r_cer * 0.6, y_cer - r_cer * 0.43, y_cer - r_cer * 0.4, y_cer - r_cer * 0.34, y_cer - r_cer * 0.28,
+             y_cer - r_cer * 0.2, y_cer])
 
-        plt.fill_between(xp, yp, y2=y5 - r, color="w")
+        plt.fill_between(xp, yp, y2=y_cer - r_cer, color="w")
 
 
 def print_line():
-    
-    if(x1!=x2):
-        kx = (x1 - x2) / (y1 - y2)
-        bx = -y2 * (x1 - x2) / (y1 - y2) + x2
-        ky = (y1 - y2) / (x1 - x2)
-        by = -x2 * (y1 - y2) / (x1 - x2) + y2
-        if (y6!=x6*ky+by):
-            if ((-1) - x2) / (x1 - x2) * (y1 - y2) + y2 < (- x2) / (x1 - x2) * (y1 - y2) + y2:
+    if x_l != x_ll:
+        kx = (x_l - x_ll) / (y_l - y_ll)
+        bx = -y_ll * (x_l - x_ll) / (y_l - y_ll) + x_ll
+        ky = (y_l - y_ll) / (x_l - x_ll)
+        by = -x_ll * (y_l - y_ll) / (x_l - x_ll) + y_ll
+
+        if y_toh != x_toh * ky + by:
+            if ((-1) - x_ll) / (x_l - x_ll) * (y_l - y_ll) + y_ll < (- x_ll) / (x_l - x_ll) * (y_l - y_ll) + y_ll:
                 if bx < 0:
-                    if (y6 > x6 * ky + by):
+                    if y_toh > x_toh * ky + by:
                         plt.fill_betweenx([-10000, by], [-10000 * 1 / ky + by, 0], color="w")
                         plt.gca().add_patch(Rectangle((0, -10000), 10000, 10000, facecolor="w"))
                         plt.fill_between([bx, 0], [0, by], color="cyan")
@@ -82,7 +112,7 @@ def print_line():
                         plt.fill_betweenx([by, 10000], [0, 10000 * 1 / ky + by], color="w")
 
                 else:
-                    if (y6 > x6 * ky + by):
+                    if y_toh > x_toh * ky + by:
                         plt.fill_betweenx([-10000, by], [-10000 * 1 / ky + by, 0], color="w")
                         plt.gca().add_patch(Rectangle((0, -10000), 10000, 10000, facecolor="w"))
                         plt.fill_between([bx, 0], [0, by], color="cyan")
@@ -93,9 +123,9 @@ def print_line():
                         plt.fill_between([bx, 0], [0, by], color="cyan")
                         plt.fill_betweenx([by, 10000], [0, 10000 * 1 / ky + by], color="w")
 
-            elif ((-1) - x2) / (x1 - x2) * (y1 - y2) + y2 > (-x2) / (x1 - x2) * (y1 - y2) + y2:
+            elif ((-1) - x_ll) / (x_l - x_ll) * (y_l - y_ll) + y_ll > (-x_ll) / (x_l - x_ll) * (y_l - y_ll) + y_ll:
                 if bx < 0:
-                    if (y6 > x6 * ky + by):
+                    if y_toh > x_toh * ky + by:
                         plt.fill_betweenx([-10000, by], [-10000 * 1 / ky + by, 0], color="w")
                         plt.gca().add_patch(Rectangle((-10000, -10000), 10000, 10000, facecolor="w"))
                         plt.fill_between([bx, 0], [0, by], color="cyan")
@@ -107,7 +137,7 @@ def print_line():
                         plt.fill_betweenx([by, 10000], [0, 10000 * 1 / ky + by], color="w")
 
                 else:
-                    if (y6 > x6 * ky + by):
+                    if y_toh > x_toh * ky + by:
                         plt.fill_betweenx([-10000, by], [-10000 * 1 / ky + by, 0], color="w")
                         plt.gca().add_patch(Rectangle((-10000, -10000), 10000, 10000, facecolor="w"))
                         plt.fill_between([bx, 0], [0, by], color="cyan")
@@ -118,41 +148,41 @@ def print_line():
                         plt.fill_between([bx, 0], [0, by], color="cyan")
                         plt.fill_betweenx([by, 10000], [0, 10000 * 1 / ky + by], color="w")
     else:
-        if(x6 > x1):
-            plt.gca().add_patch(Rectangle((x6, 10000), 20000, -20000, facecolor="w"))
+        if x_toh > x_l:
+            plt.gca().add_patch(Rectangle((x_toh, 10000), 20000, -20000, facecolor="w"))
         else:
-            plt.gca().add_patch(Rectangle((x6, 10000), -20000, -20000, facecolor="w"))
+            plt.gca().add_patch(Rectangle((x_toh, 10000), -20000, -20000, facecolor="w"))
 
 
 def print_recctangle():
+    if (min(x_rec, x_recc) <= x_toh <= max(x_rec, x_recc)) and (min(y_rec, y_recc) <= y_toh <= max(y_rec, y_recc)):
 
-    if (min(x3, x4) <= x6 <= max(x3, x4)) and (min(y3, y4) <= y6 <= max(y3, y4)):
-
-        plt.gca().add_patch(Rectangle((-10000, max(y3,y4)), 20000, 20000, facecolor="w"))
-        plt.gca().add_patch(Rectangle((-10000, max(y3,y4)), min(x3,x4)+10000, -20000, facecolor="w"))
-        plt.gca().add_patch(Rectangle((max(x3,x4), max(y3, y4)), 20000, -20000, facecolor="w"))
-        plt.gca().add_patch(Rectangle((min(x3,x4), min(y3,y4)), 20000, -20000, facecolor="w"))
+        plt.gca().add_patch(Rectangle((-10000, max(y_rec, y_recc)), 20000, 20000, facecolor="w", color="r"))
+        plt.gca().add_patch(Rectangle((-10000, max(y_rec, y_recc)), min(x_rec, x_recc) + 10000, -20000, facecolor="w", color="r"))
+        plt.gca().add_patch(Rectangle((max(x_rec, x_recc), max(y_rec, y_recc)), 20000, -20000, facecolor="w", color="r"))
+        plt.gca().add_patch(Rectangle((min(x_rec, x_recc), min(y_rec, y_recc)), 20000, -20000, facecolor="w", color="r"))
         print(1)
     else:
-        plt.gca().add_patch(Rectangle(([min(x3, x4), min(y3, y4)]), l, d, facecolor="w"))
+        plt.gca().add_patch(Rectangle(([min(x_rec, x_recc), min(y_rec, y_recc)]), l_tangle, d_tangle, facecolor="w", color="r"))
         print(2)
 
 
-if x1 == x2:
-    line([x1, x1], [-10000, 10000])
+if x_l == x_ll:
+    line([x_l, x_l], [-10000, 10000])
 else:
-    line([-10000, 10000], [(-10000-x2)/(x1-x2)*(y1-y2)+y2, (10000-x2)/(x1-x2)*(y1-y2)+y2])
+    line([-10000, 10000],
+         [(-10000 - x_ll) / (x_l - x_ll) * (y_l - y_ll) + y_ll, (10000 - x_ll) / (x_l - x_ll) * (y_l - y_ll) + y_ll])
 
 print_line()
 print_circle()
 print_recctangle()
 
-x, y = [x1, x2], [y1, y2]
+x, y = [x_l, x_ll], [y_l, y_ll]
 line(x, y)
 
-x = [min(x3, x4), min(y3, y4)]
-rectangle(x, l, d)
-x = [x5, y5]
-circle(x, r)
-plt.gca().scatter(x6, y6)
+x = [min(x_rec, x_recc), min(y_rec, y_recc)]
+rectangle(x, l_tangle, d_tangle)
+x = [x_cer, y_cer]
+circle(x, r_cer)
+plt.gca().scatter(x_toh, y_toh)
 plt.show()
